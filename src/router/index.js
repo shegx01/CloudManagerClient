5115fa032f27d8558fe881c1d84669cd18512ecd
O O Sam <shegx01@gmail.com>
2020-06-30 15:38:45 +0100

    monthly and highest spending card

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
+import CostManagement from '@/views/CostManagement.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Dashboard',
     component: Dashboard
+  },
+  {
+    path: '/cost-management',
+    name: 'CostManagement',
+    component: CostManagement
   }
 ]
 
