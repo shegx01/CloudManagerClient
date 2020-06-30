45c82d536ecebd173c7a6976f45fe0de4b0ca6fa
O O Sam <shegx01@gmail.com>
2020-06-30 16:11:20 +0100

    installed chart library

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/css/styles.css'
+// echarts installation
+
+import Echarts from 'vue-echarts'
 
 Vue.config.productionTip = false
 
+Vue.component('v-echart', Echarts)
 new Vue({
   router,
   render: h => h(App)
